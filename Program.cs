3b037671a2ac9865134bdec14b3aa64a3566a67f
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-12 15:56:42 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4.OOPEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Encapsulation is achieved by access modifiers !
+           // OOPEncapsulation hides the implementation Details !
+           // class anounces sime operation to its users ! --its public interface ;
+           // all data  members(fields) -> fields should be hidden !
+           // accessed via properties(read -only or read and write )
+           // no interface members should be hidden, they should be public
+           // properties return -get or set the value of the field !
+           //fields are declared private;- > not accesible from the outside world !
+           // Constructors are almost always  declared public, 
+           //interfaces  methds are declared public, but not explicitely declared as public
+           // non-interface methods- are  private/protected 
+           // Encapsulaton  hides from the world  what it does not need to know!, 
+           // encapsulation is realized though access modifiers, and gives a logic when accessing data !
+           // Encapsulation hides the implementation and gives a better maintenance !
+        }
+    }
+}
